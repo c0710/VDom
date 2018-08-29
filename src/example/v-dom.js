f20495eb584add2ebd2014beea8d749c044ea4c8
c0710 <c0710@foxmail.com>
2018-08-29 16:48:33 +0800

    props diff

--- src/example/v-dom.js
+++ src/example/v-dom.js
@@ -14,7 +14,7 @@ function Element(tagName, props, children) {
 Element.prototype.render = function () {
     // 根据tagName构建Dom节点
     var el = document.createElement(this.tagName);
-    var props = this.props;
+    var props = this.props || {};
     // 为Dom节点添加属性
     for (var k in props) {
         var v = props[k];
@@ -34,10 +34,80 @@ Element.prototype.render = function () {
     return el;
 };
 
-var ul = el('ul', {id: 'list'}, [
-    el('li', {class: 'item'}, ['item1']),
-    el('li', {class: 'item'}, ['item2']),
-    el('li', {class: 'item'}, ['item3']),
-]);
+var REPLACE = 0,    // 替换掉原来的节点，例如把上面的div换成了span
+    REORDER = 1,    // 移动、删除、新增子节点
+    PROPS = 2,      // 修改了节点的属性
+    TEXT = 3;       // 修改文本内容
 
-document.body.appendChild(ul.render())
+// diff函数， 对比两棵树
+function diff(oldTree, newTree) {
+    var index = 0;
+    var patches = {};
+
+    dfsWalk(oldTree, newTree, index, patches);
+
+    return patches
+}
+
+// 深度优先遍历
+function dfsWalk(oldNode, newNode, index, patches) {
+    var currentPatch = [];
+
+    if (newNode === null) { // 节点被删除
+
+    } else if (typeof oldNode === 'string' && typeof newNode === 'string') { // 文字节点改变
+        if (newNode !== oldNode) {
+            currentPatch.push({type: TEXT, content: newNode})
+        }
+    }
+}
+
+function diffProps(oldNode, newNode) {
+    var count = 0;
+    var oldProps = oldNode.props,
+        newProps = newNode.props;
+
+    var key, value;
+    var propsPatches = {};
+
+    for (key in oldProps) {
+        value = oldProps[key];
+        if (newProps[key] !== value) {
+            count++;
+            propsPatches.key = newProps[key];
+        }
+    }
+
+    if (!count) {
+        return null
+    }
+
+    return propsPatches
+}
+
+
+
+
+
+
+
+
+
+
+
+
+var tree = el('div', {'id': 'container'}, [
+    el('h1', {style: 'color: blue'}, ['simple virtal dom']),
+    el('p', null, ['Hello, virtual-dom']),
+    el('ul', null, [el('li', {class: 'item'})])
+])
+
+
+document.body.appendChild(tree.render());
+
+// 3. 生成新的虚拟DOM
+var newTree = el('div', {'id': 'container'}, [
+    el('h1', {style: 'color: red'}, ['simple virtal dom']),
+    el('p', null, ['Hello, virtual-dom']),
+    el('ul', null, [el('li'), el('li')])
+])
